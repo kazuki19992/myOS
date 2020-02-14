11dde20af15bb7c6922af06180e91693bc64e412
kazuki19992 <kushida98@gmail.com>
2020-02-15 00:26:30 +0900

    Day4おわり！やっとOSぽい画面になった！！

--- myOS/Day04/bootpack.c
+++ myOS/Day04/bootpack.c
@@ -1,4 +1,23 @@
 // bootpack.c
+
+// カラーパレットで設定した色を定数として宣言する
+#define COL8_000000      0
+#define COL8_FF0000      1
+#define COL8_00FF00      2
+#define COL8_FFFF00      3
+#define COL8_0000FF      4
+#define COL8_FF00FF      5
+#define COL8_00FFFF      6
+#define COL8_FFFFFF      7
+#define COL8_C6C6C6      8
+#define COL8_840000      9
+#define COL8_008400     10
+#define COL8_848400     11
+#define COL8_000084     12
+#define COL8_840084     13
+#define COL8_008484     14
+#define COL8_848484     15
+
 extern void io_hlt(void);
 extern void io_cli(void);
 extern void io_out8(int port, int data);
@@ -9,21 +28,46 @@ extern void io_store_eflags(int eflags);
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+
 void HariMain(void) {
-    int i;
-    char *p = (char *) 0xa0000;    // ポインタ変数pは、BYTE[...]用の番地
+    int xsize = 320, ysize = 200;
+    char *vram = (char *) 0xa0000;    // ポインタ変数pは、BYTE[...]用の番地
     // ↑ポインタ変数と呼ばずに"番地変数"と呼んだほうがいいかも？？
 
     init_palette();     // パレットを設定
 
-    for (i = 0; i <= 0xffff; i++){
-        p[i] = i & 0x0f;
-    }
+    boxfill8(vram, xsize, COL8_008484,  0,         0,          xsize -  1, ysize - 29);
+    boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 28, xsize -  1, ysize - 28);
+    boxfill8(vram, xsize, COL8_FFFFFF,  0,         ysize - 27, xsize -  1, ysize - 27);
+    boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 26, xsize -  1, ysize -  1);
 
+    boxfill8(vram, xsize, COL8_FFFFFF,  3,         ysize - 24, 59,         ysize - 24);
+    boxfill8(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
+    boxfill8(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
+    boxfill8(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
+    boxfill8(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
+    boxfill8(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
+
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+    boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+    
     // for(;;)ってwhile(1)でよくね？？
     while (1) {
         io_hlt();
     }
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1){
+    int x, y;
+    for(y = y0; y <= y1; y++){
+        for(x = x0; x <= x1; x++){
+            vram[y * xsize + x] = c;
+        }
+    }
+    return;
 }
 
 void init_palette(void){
