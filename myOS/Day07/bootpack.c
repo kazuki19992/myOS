c81f41b3553f058c47cd78fee8ce70586a557f02
kazuki19992 <kushida98@gmail.com>
2020-02-18 22:01:48 +0900

    hikaliumさんのz_toolsをいれました

--- myOS/Day07/bootpack.c
+++ myOS/Day07/bootpack.c
@@ -2,8 +2,7 @@
 #include "bootpack.h"
 #include <stdio.h>
 
-void HariMain(void)
-{
+void HariMain(void){
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
 	char s[40], mcursor[256];
 	int mx, my;
--- myOS/Day07/bootpack.h
+++ myOS/Day07/bootpack.h
@@ -9,6 +9,10 @@ struct BOOTINFO { /* 0x0ff0-0x0fff */
 };
 #define ADR_BOOTINFO	0x00000ff0
 
+
+// bootpack.c
+void HariMain(void);
+
 /* naskfunc.nas */
 void io_hlt(void);
 void io_cli(void);
